4a8232ef56988568f773c6807345d00081d03c84
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-09-04 17:51:51 -0500

    feat: call to appsync

--- teems-round-manager/index.js
+++ teems-round-manager/index.js
@@ -1,6 +1,6 @@
 import * as utils from "./utils.js";
 import * as handlers from "./handlers.js";
-import fetch from "node-fetch";
+import { default as fetch, Request } from "node-fetch";
 
 export async function handler(event) {
   // TODO implement
@@ -10,6 +10,19 @@ export async function handler(event) {
     case "/completeRound":
       return handlers.completeRound(event.body);
     default:
-      return utils.respBuilder(400, { error: `the path "${event.path}" does not exist` });
+      const query = `query MyQuery {
+          listVenues {
+            items {
+              id
+            }
+          }
+        }`;
+
+      const responseJson = await utils.appSyncRequestMaker({ query });
+
+      return utils.respBuilder(400, {
+        error: `the path "${event.path}" does not exist`,
+        responseJson
+      });
   }
 }
--- teems-round-manager/utils.js
+++ teems-round-manager/utils.js
@@ -1,5 +1,21 @@
+import { default as fetch, Request } from "node-fetch";
+
 export const respBuilder = (statusCode, body, additionalItems) => ({
   statusCode,
   body: JSON.stringify(body),
   ...additionalItems
 });
+
+export const appSyncRequestMaker = async (body) => {
+  const request = new Request(process.env.APPSYNC_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "x-api-key": process.env.APPSYNC_API_KEY
+    },
+    body: JSON.stringify(body)
+  });
+
+  const response = await fetch(request);
+  const responseJson = await response.json();
+  return responseJson;
+};
